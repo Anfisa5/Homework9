d9f8de5b662586975e4039064445c00dadde5055
Anfisa Golovanova <anfisa.faa@gmail.com>
2022-11-24 20:25:18 +0700

    add file and file gitignore

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,88 @@
+package ru.netology;
+
+public class Radio {
+
+
+    // ТЕКУЩАЯ РАДИОСТАНЦИЯ
+    private int currentRadioStation;
+
+    public int getCurrentRadioStation() {
+
+        return currentRadioStation;
+    }
+
+    // 1.Номер текущей радиостанции может принимать значения только в пределах от 0 до 9.
+
+    public void setCurrentRadioStation(int newCurrentRadioStationStation) {
+
+        if (newCurrentRadioStationStation < 0) {
+            return;
+        }
+        if (newCurrentRadioStationStation > 9) {
+            return;
+        }
+        this.currentRadioStation = newCurrentRadioStationStation;
+    }
+
+    // 2.МЕТОД NEXT: если текущая радиостанция 9 и клиент нажал на кнопку next на пульте,то текущей должна стать 0.
+    // В остальных случаях при нажатии на эту же кнопку радио переключается просто на следующую станцию.
+    public void next() {
+        if (currentRadioStation < 9) {
+            currentRadioStation++;
+        } else {
+            currentRadioStation = 0;
+        }
+    }
+
+    // 3.МЕТОД PREV: Если текущая радиостанция 0 и клиент нажал на кнопку prev на пульте, то текущей должна стать 9.
+    // в остальных случаях радио переключается просто на предыдущую станцию.
+    public void prev() {
+
+        if (currentRadioStation > 0) {
+            currentRadioStation--;
+        } else {
+            currentRadioStation = 9;
+        }
+    }
+
+
+    // 4.Клиент должен иметь возможность выставлять номер радиостанции через прямое указание её номера.
+    // Для этого подойдёт один метод-сеттер с проверкой на допустимость номера станции.
+    // см.п.1 <<ТЕКУЩАЯ РАДИОСТАНЦИЯ>>
+
+
+    // УРОВЕНЬ ГРОМКОСТИ ЗВУКА
+    private int currentVolume;
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+
+    // 1.Клиент должен иметь возможность увеличивать и уменьшать уровень громкости звука в пределах от 0 до 10
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        this.currentVolume = newCurrentVolume;
+    }
+
+    //2. МЕТОД УВЕЛИЧЕНИЯ ГРОМКОСТИ ЗВУКА НА ОДИН: если уровень громкости звука достиг максимального значения,
+    //то дальнейшее нажатие на +, не должно ни к чему приводить.
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    //3.МЕТОД УМЕНЬШЕНИЯ ГРОМКОСТИ ЗВУКА НА ОДИН: если уровень громкости звука достиг минимального значения,
+    // то дальнейшее нажатие на -, не должно ни к чему приводить.
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+}
